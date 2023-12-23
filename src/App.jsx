dc706ec862141182d0bb9d659348f9c05ad82d08
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-12-23 15:15:49 +0800

    Add the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Card from './component/Card'
+
 
 function App() {
+
+const learningCards = [
+    {
+      title : "HTML & CSS foundations",
+      description : "These languages are the backbone of every website, defining structure, content, and presentation.",
+      cover: "../src/assets/images/illustration-article.svg",
+      category : "Learning",
+      publishedDay : 21,
+      publishedMonth : 12,
+      publishedYear : 2023,
+      author: "Greg Hoopper",
+      authorPhoto: "../src/assets/images/image-avatar.webp",
+    }
+]
+
   return (
     <div className="bg-yellow w-screen h-screen">
+      {learningCards.map((card, index) => (
+        <Card key={index} card={card} />
+
+      ))}
+
       
     </div>
   )
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
+
+const Card = ({ card }) => {
+  const monthFormat = (monthNumber) => {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return months[monthNumber - 1];
+  };
+
   return (
     <div className="">
       <div>
-
+        <img src={card.cover} alt={card.title} />
+        <h3>{card.category}</h3>
+        <p>
+          Published {card.publishedDay} {monthFormat(card.publishedMonth)}{" "}
+          {card.publishedYear}
+        </p>
+        <h1>{card.title}</h1>
+        <p>{card.description}</p>
+        <div>
+          <img src={card.authorPhoto} alt={card.author} />
+          <h3>{card.author}</h3>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
